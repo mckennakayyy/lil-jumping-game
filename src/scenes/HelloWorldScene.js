00da1affe07d51d05317137dd299b1b44c9b64d2
McKenna Warren <mckennakayyy@gmail.com>
2021-06-25 19:37:39 -0400

    feat: initializes gameplay

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -9,31 +9,33 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
+        // this.load.setBaseURL('http://labs.phaser.io')
 
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.image('sky', 'assets/forest.png')
+        this.load.image('logo', 'assets/logo.png')
+        this.load.image('name', 'assets/name.png')
     }
 
     create()
     {
         this.add.image(400, 300, 'sky')
 
-        const particles = this.add.particles('red')
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
 
         const logo = this.physics.add.image(400, 100, 'logo')
+        logo.scale = 4
 
-        logo.setVelocity(100, 200)
+        logo.setVelocity(50, 100)
         logo.setBounce(1, 1)
         logo.setCollideWorldBounds(true)
 
-        emitter.startFollow(logo)
+        const name = this.physics.add.image(100, 400, 'name')
+        name.scale = 4
+
+        name.setVelocity(50, 100)
+        name.setBounce(1, 1)
+        name.setCollideWorldBounds(true)
+
+
+
     }
 }
